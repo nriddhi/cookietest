d9038a8b8982a9cb17789d5a97d59362e31d281d
Nayeem Hyder Riddhi <riddhi00121@gmail.com>
2023-05-25 19:55:44 +0600

    comiit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: true,
+    origin: 'https://www.nayeemriddhi.info/cookieapp',
     credentials: true
 }));
 
