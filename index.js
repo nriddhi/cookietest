c64abeff54f8d9abf9b6d4f529571facb5bb1845
Nayeem Hyder Riddhi <riddhi00121@gmail.com>
2023-05-25 21:09:55 +0600

    commit

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(function (req, res, next) {
+    res.header("Content-Type", "application/json;charset=UTF-8");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 app.use('/api/auth', authRoute);
 
 mongoose.connect(process.env.MONGO_URL)
